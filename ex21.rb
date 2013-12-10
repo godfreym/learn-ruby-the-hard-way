62534919a18fee96e3c878f729f623045f0e1e08
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-10 15:13:14 +0000

    defining the functions and declaring variables that calls the defined functions

--- ex21.rb
+++ ex21.rb
@@ -2,3 +2,25 @@ def add(a,b)
  puts "Adding #{a} + #{b}"
  a + b
  end
+
+ def subtract(a,b)
+ 	puts "Subtracting #{a} - #{b}"
+ 	a - b
+ end
+
+ def multiply(a,b)
+ 	puts "multiplying #{a} * #{b}"
+ 	a * b
+ end
+
+ def divide (a,b)
+ 	puts "Dividing #{a} / #{b}"
+ 	a / b
+ end
+
+ puts "Let's us do some math with just functions!"
+ age = add(30,5)
+ height = subtract(78,4)
+ weight = multiply(90,2)
+ iq = divide(100,2)
+ 
