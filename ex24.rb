57c42ee0f1b420b5a17047d8f94ddcd0a3c4d8b9
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-10 19:06:05 +0000

    printing some lines and a poem

--- ex24.rb
+++ ex24.rb
@@ -1,1 +1,19 @@
-puts ""
+puts "Let's practice everything"
+puts "You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs."
+poem = <<MULTI_LINE_STRING
+
+\t The lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\twhere there is none.
+
+MULTI_LINE_STRING
+
+puts "---------------"
+puts poem
+puts "---------------"
+
+five = 10 - 2 + 3 - 6
+puts "This should be five: #{five}"
