0d4ee6d2c67a139544abdec3ee327b24eda9550f
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-10 12:57:45 +0000

    adding functions to take several arguments

--- ex18.rb
+++ ex18.rb
@@ -1,1 +1,29 @@
 # this one is like your script with argv
+
+def puts_two(*args)
+	  arg1, arg2 = args
+puts "arg1: #{arg1}, arg2: #{arg2}" 
+end
+
+# ok, that *args ia actually pointless, we can just do this
+
+def puts_two_again (arg1,arg2)
+	puts "arg1: #{arg1}, arg2: #{arg2}" 
+end
+
+# this just take one argument
+
+def puts_one(arg1)
+	puts puts "arg1: #{arg1}"
+end
+
+# this one takes no argument
+
+def puts_none()
+ puts "I got nothing'. "
+ end 
+
+ puts_two('Zed','Shaw')
+ puts_two_again("Godfrey","Mlowe")
+ puts_one("First!")
+ puts_none()
