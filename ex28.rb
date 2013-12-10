dae87e1e73dd34d62a6522aad6740f599e95b61c
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-10 22:30:42 +0000

    adding more boolean logic and printing the result

--- ex28.rb
+++ ex28.rb
@@ -1,2 +1,9 @@
 puts true and true
 puts false and true
+puts (1 == 1)and (2 == 1)
+puts "test" == "test"
+puts false and 0!=0
+puts "test" == "testing"
+puts 1 != 0 and 3 == 1 
+puts "TEST" == 1
+puts "chunky" == "bacon" and not (3 == 4 or 3 == 3)
