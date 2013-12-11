297150346a4d16c0e382c820faab7d426dbffc3f
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-11 11:42:57 +0000

    declaring insanity and assigning it to the value to be provided by user

--- ex31.rb
+++ ex31.rb
@@ -25,11 +25,13 @@ def prompt
   	puts "2. Yellow jacket clothespins."
   	puts "3. Understanding revolvers yelling melodies."
 
-  	prompt; insanity == "1" or insanity == "2"
+  	prompt; insanity=gets.chomp()
+  	if insanity == "1" or insanity == "2"
   	puts "Your body survives powered by a mind of jello. Good job!"
   else
   	puts "The insanity rots your eyes into a pool of muck. Good job!"
   end
+
 else
 	puts "You stumble around and fall on a knife and die. Good job!"
 end
