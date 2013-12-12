82b2601535907e3e6bae6dd852c79396f68610a0
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 14:53:10 +0000

    creating a module and then access its function

--- ex40.rb
+++ ex40.rb
@@ -1,1 +1,11 @@
+# this code is present in another file mystuff.rb
 module MyStuff
+	def MyStuff.apple()
+		 puts "I AM APPLES!"
+	end
+end
+  # module ends here^
+
+  # You can access apple as follows
+  require 'mystuff'
+   MyStuff.apple()
