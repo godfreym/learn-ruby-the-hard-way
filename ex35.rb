2980822b3e5977eb026f923e8b75602840d56178
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 12:38:19 +0000

    adding condition which will move bear away from door

--- ex35.rb
+++ ex35.rb
@@ -31,7 +31,7 @@ def bear_room()
 
         if next_move == "take honey"
         	dead("The bear looks at you and then slaps your face off.")
-        elsif next_move == "taunt bear" and not bear_moved
+        elsif next_move == "taunt bear" or "kill" or "kill bear" and not bear_moved
         	puts "The bear has moved from the door. You can go through it now."
         	bear_moved = true
         elsif next_move == "taunt bear" and bear_moved
