a0219fb7fa67ffe46575d184b006e79e5b0398b8
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 14:56:16 +0000

    creating a variable within the module and calling it from another ruby file

--- ex40.rb
+++ ex40.rb
@@ -3,9 +3,12 @@ module MyStuff
 	def MyStuff.apple()
 		 puts "I AM APPLES!"
 	end
+	# this is just a variable
+	TANGERINE = "Living reflection of dream"
 end
   # module ends here^
 
   # You can access apple as follows
   require 'mystuff'
    MyStuff.apple()
+   puts MyStuff::TANGERINE #this is the way to call a module's variable in separate file
