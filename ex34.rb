f23b959717db3d93b740fe72636074de69663bfe
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 07:34:59 +0000

    priting the last animal by method .last

--- ex34.rb
+++ ex34.rb
@@ -1,3 +1,4 @@
 animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 puts "The first animal is #{animals[0]}"
 puts "The second animal is #{animals[1]}"
+puts "The last animal is " + animals.last
