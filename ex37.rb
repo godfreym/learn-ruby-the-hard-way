c98b2338f3ec96bcbe84c1b092a0b6965fe3a07a
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 13:11:07 +0000

    adding data types for ruby

--- ex37.rb
+++ ex37.rb
@@ -1,5 +1,57 @@
+# Below are keywords used by ruby
 alias
 and
 BEGIN
 begin
 break
+alias
+    and
+    BEGIN
+    begin
+    break
+    case
+    class
+    def
+    defined?
+    do
+    else
+    elsif
+    END
+    end
+    ensure
+    false
+    for
+    if
+    in
+    module
+    next
+    nil
+    not
+    or
+    redo
+    rescue
+    retry
+    return
+    self
+    super
+    then
+    true
+    undef
+    unless
+    until
+    when
+    while
+    yield
+
+# Below are Data Types used by ruby
+
+    true
+    false
+    nil
+    constants
+    strings
+    numbers
+    ranges
+    arrays
+    hashes
+
