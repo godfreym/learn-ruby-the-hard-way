e2741d13f075df744191dd30803debb87d6e71d0
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 15:17:06 +0000

    creating objects and instantiate them and call the methods from class

--- ex40.rb
+++ ex40.rb
@@ -42,4 +42,13 @@ end
   	end
   end
 
-  
+  happy_bday = Song.new(["Happy birthday to you",
+  	                     "I dont want to get sued",
+  	                     "So I will stop right there"])
+
+  bulls_on_parade = Song.new(["They rally around the family",
+  	                          "With pockets full of shells"])
+
+ happy_bday.sing_me_a_song()
+
+ bulls_on_parade.sing_me_a_song()
