d7d853e795f65c6e2598a1181d83fbb19c079ad2
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 08:22:54 +0000

    defining other function

--- ex35.rb
+++ ex35.rb
@@ -29,7 +29,27 @@ def bear_room()
 	while true
 		prompt; next_move = gets.chomp()
 
-		if next_move.include? "flee"
+        if next_move == "take honey"
+        	dead("The bear looks at you and then slaps your face off.")
+        elsif next_move == "taunt bear" and not bear_moved
+        	puts "The bear has moved from the door. You can go through it now."
+        	bear_moved = true
+        elsif next_move == "taunt bear" and bear_moved
+        	dead("The bear gets pissed off and chews your leg off.")
+        elsif next_move == "open door" and bear_moved
+        	gold_room()
+        else
+        	puts "I got no idea what that means."
+        end
+    end
+end
+
+def cthulhu_room()
+	puts "Here you see the great evil Cthulhu."
+	puts "He, it, whatever stares at you and you go insane."
+	puts "Do you flee for your life or eat your head?"
+	prompt; next_move = gets.chomp()
+      if next_move.include? "flee"
 			start()
 			elsif next_move.include? "head"
 				dead("Well that was tasty!")
