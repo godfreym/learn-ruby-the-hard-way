46ef906cd32942290c8b9d91e6d2b6d1d855a645
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-12 15:06:11 +0000

    how to get things from things

--- ex40.rb
+++ ex40.rb
@@ -12,3 +12,18 @@ end
   require 'mystuff'
    MyStuff.apple()
    puts MyStuff::TANGERINE #this is the way to call a module's variable in separate file
+
+   # GETTING THINGS FROM THINGS
+   # There are 3 ways to get things from things
+  
+  # 1. hash style
+  mystuff['apples']
+
+  # 2. module style
+  mystuff.apples()
+  puts mystuff.tangerine
+  
+  # 3. class style
+  thing = MyStuff.new()
+  thing.apples()
+  puts thing.tangerine
