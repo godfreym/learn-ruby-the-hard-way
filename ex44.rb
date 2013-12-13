74c44e0639226229bf7fb72412408d0de9ece1a1
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-13 09:47:12 +0000

    defining super and sub class and objects for those classes

--- ex44.rb
+++ ex44.rb
@@ -1,1 +1,28 @@
 class Parent
+	def override()
+		puts "PARENT override()"
+	end
+
+	def implicity()
+		puts "PARENT implicity()"
+	end
+
+	def altered()
+		puts "PARENT altered()"
+	end
+end
+
+class Child < Parent
+	def override()
+		puts "CHILD override()"
+	end
+
+	def altered()
+		puts "CHILD, BEFORE PARENT altered()"
+		super()
+		puts "CHILD, AFTER PARENT altered()"
+	end
+end
+
+dad = Parent.new()
+son = Child.new()
