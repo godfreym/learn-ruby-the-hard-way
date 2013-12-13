3628ff63bae7dfc50dca2d8d931561ae3fc92d46
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-13 11:00:33 +0000

    implementing properties sharing by the use of module

--- ex44.rb
+++ ex44.rb
@@ -78,3 +78,42 @@ puts "\n-----composition results------"
 son.implicit()
 son.override()
 son.altered()
+
+
+# ----module implementation-----
+
+module Other
+	def Other.override()
+		puts "OTHER override()"
+	end
+
+	def Other.implicit()
+		puts "OTHER implicit()"
+	end
+
+	def Other.altered()
+		puts "OTHER altered()"
+	end
+end
+
+class Child
+	def implicit()
+		Other.implicit()
+	end
+
+	def override()
+		puts "CHILD override()"
+	end
+
+	def altered()
+		puts "CHILD, BEFORE OTHER altered()"
+		Other.altered()
+		puts "CHILD, AFTER OTHER altered()"
+	end
+end
+
+son = Child.new()
+puts "\n-------module results-------"
+son.implicit()
+son.override()
+son.altered()
