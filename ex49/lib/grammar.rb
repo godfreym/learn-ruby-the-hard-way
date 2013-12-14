887b0a53005299346b4f21b41d488e409feb75aa
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-14 12:05:55 +0000

    adding code to parse simple sentences using the lexicon class

--- ex49/lib/grammar.rb
+++ ex49/lib/grammar.rb
@@ -1,37 +1,92 @@
-	class Lexicon
-           
-      def scan(stuff)
-       
-        def convert_number(stuff)
-           begin
-             Integer(stuff)
-           rescue ArgumentError
-             return nil
-           end
-        end
-       
-        hash = {'north' => 'direction', 'south' => 'direction', 'east' => 'direction',
-                 'go' => 'verb', 'kill' => 'verb', 'eat' => 'verb',
-                 'the' => 'stop', 'in' => 'stop', 'of' => 'stop',
-                 'bear' => 'noun', 'princess' => 'noun',
-                  }
-        words = stuff.split()
-         
-        words.map do |word|
-          if convert_number(word)
-            classification = 'number'
-          elsif word != hash[word]
-            classification = 'error'
-          else
-            classification = hash[word].to_sym
-          end
-         
-          Pair.new(classification, word)
-     
-        end
-      end
+class ParserError < Exception
+
+end
+
+class Sentence
+
+  def initialize(subject, verb, object)
+    # remember we take Pair.new(:noun, "princess") structs and convert them
+    @subject = subject.word
+    @verb = verb.word
+    @object = object.word
+  end
+
+end
+
+module Parser
+
+  def self.peek(word_list)
+    begin
+      word_list.first.token
+    rescue
+      nil
     end
-    Pair = Struct.new(:token, :word)
+  end
+  
+  def self.match(word_list, expecting)
+    begin
+      word = word_list.shift
+      if word.token == expecting
+        word
+      else
+        nil
+      end
+    rescue
+      nil
+    end
+  end
+  
+  def self.skip(word_list, token)
+    while peek(word_list) == token
+      match(word_list, token)
+    end
+  end
+  
+  def self.parse_verb(word_list)
+    skip(word_list, :stop)
+  
+    if peek(word_list) == :verb
+      return match(word_list, :verb)
+    else
+      raise ParserError.new("Expected a verb next.")
+    end
+  end
+  
+  def self.parse_object(word_list)
+    skip(word_list, :stop)
+    next_word = peek(word_list)
+  
+    if next_word == :noun
+      return match(word_list, :noun)
+    end
+    if next_word == :direction
+      return match(word_list, :direction)
+    else
+      raise ParserError.new("Expected a noun or direction next.")
+    end
+  end
+  
+  def self.parse_subject(word_list, subj)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
+  
+    return Sentence.new(subj, verb, obj)
+  end
+  
+  def self.parse_sentence(word_list)
+    skip(word_list, :stop)
+  
+    start = peek(word_list)
+  
+    if start == :noun
+      subj = match(word_list, :noun)
+      return parse_subject(word_list, subj)
+    elsif start == :verb
+      # assume the subject is the player then
+      return parse_subject(word_list, Pair.new(:noun, "player"))
+    else
+      raise ParserError.new("Must start with subject, object, or verb not: #{start}")
+    end
+  end
 
-    # The code should be fixed for 6 failures
-
+end
