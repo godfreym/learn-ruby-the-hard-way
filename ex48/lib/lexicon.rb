4d99013f72ed776c624aa462670a8727a5cd65a0
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-14 11:13:03 +0000

    creating a lexicon file

--- ex48/lib/lexicon.rb
+++ ex48/lib/lexicon.rb
@@ -1,17 +1,35 @@
-class Room 
-	attr_accessor :name, :description, :paths
+	class Lexicon
+           
+      def scan(stuff)
+       
+        def convert_number(stuff)
+           begin
+             Integer(stuff)
+           rescue ArgumentError
+             return nil
+           end
+        end
+       
+        hash = {'north' => 'direction', 'south' => 'direction', 'east' => 'direction',
+                 'go' => 'verb', 'kill' => 'verb', 'eat' => 'verb',
+                 'the' => 'stop', 'in' => 'stop', 'of' => 'stop',
+                 'bear' => 'noun', 'princess' => 'noun',
+                  }
+        words = stuff.split()
+         
+        words.map do |word|
+          if convert_number(word)
+            classification = 'number'
+          elsif word != hash[word]
+            classification = 'error'
+          else
+            classification = hash[word].to_sym
+          end
+         
+          Pair.new(classification, word)
+     
+        end
+      end
+    end
+    Pair = Struct.new(:token, :word)
 
-	def initialize(name, description)
-		@name = name
-		@description = description
-		@paths = {}
-	end
-
-	def go(direction)
-		@paths[direction]
-	end
-
-	def add_paths(paths)
-		@paths.update(paths)
-	end
-end
